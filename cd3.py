87187c1d93f0208b5186766c959f4346050a7dbc
Hector Paredes <paredesramoshector@gmail.com>
2025-03-12 10:09:08 -0400

    Prueba Lab 5

---
+++ cd3.py
@@ -0,0 +1,24 @@
+import json
+
+def cargar_alumnos(ruta_archivo):
+    # Abre el archivo JSON y carga el contenido
+    with open(ruta_archivo, 'r') as archivo:
+        return json.load(archivo)
+
+def buscar_alumno(alumnos, nombre):
+    # Busca el alumno por nombre
+    for alumno in alumnos:
+        if alumno['nombre'].lower() == nombre.lower():
+            return alumno
+    return None
+
+if _name_ == "_main_":
+    ruta = 'lab5/alumnos.json'  # Asegúrate de usar la ruta correcta
+    alumnos = cargar_alumnos(ruta)
+    nombre_buscar = input("Ingrese el nombre del alumno a buscar: ")
+    resultado = buscar_alumno(alumnos, nombre_buscar)
+    
+    if resultado:
+        print(f"Alumno encontrado: {resultado}")
+    else:
+        print("Alumno no encontrado.")
